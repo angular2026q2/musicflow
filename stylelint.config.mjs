d4eb85965a4785ce123464978390d0250de0616e
cherkasovaa <alina.ch1202@gmail.com>
2026-05-23 23:41:23 +0300

    fix: stylelint config and styles

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ['stylelint-config-standard', 'stylelint-config-clean-order'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-clean-order'],
   rules: {
     'no-empty-source': null, // Allow empty CSS files. Temporally rule.
   },
@@ -8,27 +8,6 @@ export default {
     {
       files: ['**/*.scss'],
       rules: {
-        'selector-pseudo-element-no-unknown': [
-          true,
-          {
-            ignorePseudoElements: ['ng-deep'],
-          },
-        ],
-
-        'at-rule-no-unknown': [
-          true,
-          {
-            ignoreAtRules: ['use', 'forward', 'mixin', 'include'],
-          },
-        ],
-
-        'media-query-no-invalid': null,
-
-        'no-descending-specificity': null,
-        
-        // FIX: Allow double slash comments in SCSS.
-        'no-invalid-double-slash-comments': null,
-
         // FIX: Allow BEM or kebab-case naming convention for class selectors.
         'selector-class-pattern': [
           '^[a-z][a-z0-9]*(?:-[a-z][a-z0-9]*)*(?:__[a-z][a-z0-9]*(?:-[a-z][a-z0-9]*)*)?(?:--[a-z][a-z0-9]*(?:-[a-z][a-z0-9]*)*)?$',

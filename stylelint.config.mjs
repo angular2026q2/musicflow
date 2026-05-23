acf8c43854af713be66700ef0bb948d1a27ee505
solidados <geekbrains.pk@internet.ru>
2026-05-23 23:59:25 +0400

    refactor(stylelint): update selector-class-pattern to support prefixed kebab-case classes

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -26,11 +26,10 @@ export default {
 
         // FIX: Allow double slash comments in SCSS.
         'no-invalid-double-slash-comments': null,
-        'scss/comment-no-loud': null,
 
         // FIX: Allow BEM or kebab-case naming convention for class selectors.
         'selector-class-pattern': [
-          '^([a-z][a-z0-9]*)([__-][a-z0-9]+)*$',
+          '^([a-z][a-z0-9]*)([__-][a-z0-9]+)*$|^p-[a-z0-9-]+$',
           {
             message: 'Use BEM or kebab-case naming convention',
           },

6ecea2c006913c9397da95ae0ce16cc69bbbd4ea
solidados <geekbrains.pk@internet.ru>
2026-05-24 00:02:52 +0400

    refactor(stylelint): disable selector-class-pattern rule to allow flexible naming conventions

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -28,12 +28,7 @@ export default {
         'no-invalid-double-slash-comments': null,
 
         // FIX: Allow BEM or kebab-case naming convention for class selectors.
-        'selector-class-pattern': [
-          '^([a-z][a-z0-9]*)([__-][a-z0-9]+)*$|^p-[a-z0-9-]+$',
-          {
-            message: 'Use BEM or kebab-case naming convention',
-          },
-        ],
+        'selector-class-pattern': null,
       },
     },
   ],

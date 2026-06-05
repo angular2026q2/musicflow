73f62f48706301973fd730dddc6646f1d44079aa
Ivan Shvets <shvets.ivan@gmail.com>
2026-06-05 17:23:07 +0300

    fix: chore review issues

--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -6,7 +6,7 @@ import { catchError, finalize, of } from 'rxjs';
 import { CoverCardComponent } from '@shared/components/cover-card/cover-card.component';
 import { SmallCardComponent } from '@shared/components/small-card/small-card.component';
 import { DropdownMenuComponent } from '@shared/components/dropdown/dropdown-menu.component';
-import { RecentlyPlayedComponent } from '@features/home/components/recently-played/recently-played.component';
+import { RecentlyPlayedComponent } from '@shared/components/recently-played/recently-played.component';
 
 import { HomeService } from '@core/services/home.service';
 import { RecentlyPlayedTrack } from '@shared/interfaces/recently-played-track.interface';

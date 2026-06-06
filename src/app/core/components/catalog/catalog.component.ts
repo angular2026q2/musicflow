e4ab8e472658a80d3892bc55694d680e381bc41e
cherkasovaa <alina.ch1202@gmail.com>
2026-06-06 11:29:38 +0300

    refactor: correct path with alias

--- src/app/core/components/catalog/catalog.component.ts
+++ src/app/core/components/catalog/catalog.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { LucideDynamicIcon } from '@lucide/angular';
+import { CatalogCardComponent } from '@shared/components/catalog-card/catalog-card.component';
 import { ICONS } from '@shared/constants/icons';
 import { CatalogData } from '@shared/types/catalog-data.types';
 import { Button } from 'primeng/button';
 import { MessageModule } from 'primeng/message';
-import { CatalogCardComponent } from '../../../shared/components/catalog-card/catalog-card.component';
 
 @Component({
   selector: 'app-catalog',

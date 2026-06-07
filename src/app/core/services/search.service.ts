9d20ecf2710a48f3a2fdb2302a84c0327ea64c46
Ivan Shvets <shvets.ivan@gmail.com>
2026-06-07 12:30:18 +0300

    feat: fetch data with search string

--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -1,10 +1,21 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { TrackResponse } from '@shared/interfaces/track-responce.interface';
+import { Track } from '@shared/interfaces/track.interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class HomeService {
+export class SearchService {
   private readonly baseUrl = '/api/v1';
   private http = inject(HttpClient);
+
+  searchTracks(search: string, limit = 10, offset = 0) {
+    const params = new HttpParams()
+      .set('search', search)
+      .set('limit', limit.toString())
+      .set('offset', offset.toString());
+
+    return this.http.get<TrackResponse<Track>>(`${this.baseUrl}/music/tracks`, { params });
+  }
 }
--- src/app/features/search/search.page.ts
+++ src/app/features/search/search.page.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SearchService } from '@core/services/search.service';
+import { Track } from '@shared/interfaces/track.interface';
 
 @Component({
   selector: 'app-search',
@@ -7,4 +10,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './search.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchPage {}
+export class SearchPage implements OnInit {
+  private route = inject(ActivatedRoute);
+  private searchService = inject(SearchService);
+  query = signal('');
+  results = signal<Track[]>([]);
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      const q = params.get('q') || '';
+      this.query.set(q);
+      if (q) {
+        this.searchService.searchTracks(q).subscribe((res) => {
+          this.results.set(res.data);
+        });
+      }
+    });
+  }
+}

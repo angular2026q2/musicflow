c30adbb5f47f0ee8d7cc5ed5a69b179e8587a5b6
cherkasovaa <alina.ch1202@gmail.com>
2026-06-28 06:10:44 +0300

    feat(timestep): apply duration pipe to formatted time displays

--- src/app/shared/components/timestep/timestep.component.ts
+++ src/app/shared/components/timestep/timestep.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { SliderModule } from 'primeng/slider';
 import { MusicPlayerService } from '@core/services/music-player.service';
+import { DurationPipe } from '@shared/pipes/duration.pipe';
+import { SliderModule } from 'primeng/slider';
 
 @Component({
   selector: 'app-timestep',
-  imports: [SliderModule, FormsModule],
+  imports: [SliderModule, FormsModule, DurationPipe],
   templateUrl: './timestep.component.html',
   styleUrl: './timestep.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,16 +17,10 @@ export class TimestepComponent {
   protected readonly currentTime = this.playerService.currentTime;
   protected readonly duration = this.playerService.duration;
 
-  readonly formattedCurrentTime = computed(() => this.formatTime(this.playerService.currentTime()));
-  readonly formattedDuration = computed(() => this.formatTime(this.playerService.duration()));
+  readonly formattedCurrentTime = computed(() => this.playerService.currentTime());
+  readonly formattedDuration = computed(() => this.playerService.duration());
 
   onSeek(value: number): void {
     this.playerService.seekTo(value);
-  }
-
-  formatTime(seconds: number): string {
-    const m = Math.floor(seconds / 60);
-    const s = Math.floor(seconds % 60);
-    return `${m}:${s.toString().padStart(2, '0')}`;
   }
 }
